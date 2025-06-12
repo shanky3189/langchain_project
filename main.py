cd1f6f2aa0f561171a0a834dd5d74f275eff5561
Shashank Sinha <shankshine@gmail.com>
2025-06-12 09:30:44 +0530

    read file and some update

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ import os,sys
 import numpy as np
 
 print("Hello World..!")
+print("This is my main branch..!")
+
+#Read a text file and fetch the content into a string variable
+def read_file(file_path):
+    """Reads a text file and returns its content as a string."""
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
+
+
+file_path=r'C:\Users\Shash\MyStudy\Langchain_git_learning\langchain_project\data\MyData.txt'
+content=read_file(file_path)
+print(content)
